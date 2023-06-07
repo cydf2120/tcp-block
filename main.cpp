62294412a062ae504bb0e94da21cd9d6aaee066f
cydf2120 <use06040@gmail.com>
2023-06-07 22:17:41 +0900

    remove functions used to debugging

--- main.cpp
+++ main.cpp
@@ -10,67 +10,6 @@
 #include "iphdr.h"
 #include "tcphdr.h"
 
-#include <stdio.h>
-#include <ctype.h>
- 
-#ifndef HEXDUMP_COLS
-#define HEXDUMP_COLS 16
-#endif
- 
-void hexdump(void *mem, unsigned int len)
-{
-        unsigned int i, j;
-        
-        for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++)
-        {
-                /* print offset */
-                if(i % HEXDUMP_COLS == 0)
-                {
-                        printf("0x%06x: ", i);
-                }
- 
-                /* print hex data */
-                if(i < len)
-                {
-                        printf("%02x ", 0xFF & ((char*)mem)[i]);
-                }
-                else /* end of block, just aligning for ASCII dump */
-                {
-                        printf("   ");
-                }
-                
-                /* print ASCII dump */
-                if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1))
-                {
-                        for(j = i - (HEXDUMP_COLS - 1); j <= i; j++)
-                        {
-                                if(j >= len) /* end of block, not really printing */
-                                {
-                                        putchar(' ');
-                                }
-                                else if(isprint(((char*)mem)[j])) /* printable char */
-                                {
-                                        putchar(0xFF & ((char*)mem)[j]);        
-                                }
-                                else /* other char */
-                                {
-                                        putchar('.');
-                                }
-                        }
-                        putchar('\n');
-                }
-        }
-}
- 
-#ifdef HEXDUMP_TEST
-int main(int argc, char *argv[])
-{
-        hexdump(argv[0], 20);
- 
-        return 0;
-}
-#endif
-
 #define MSG "HTTP/1.0 302 Redirect\r\nLocation: http://warning.or.kr\r\n\r\n"
 
 #pragma pack(push, 1)
@@ -90,24 +29,24 @@ void usage()
 Mac find_my_mac(char* dev)
 {	
 	struct ifreq 	ifr;
-    Mac 			my_mac;
-    int 			fd;
+	Mac 			my_mac;
+	int 			fd;
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    memset(&ifr, 0, sizeof(ifr)); 
-    strcpy(ifr.ifr_name, dev); 
+	memset(&ifr, 0, sizeof(ifr)); 
+	strcpy(ifr.ifr_name, dev); 
 
-    if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0)
+	if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0)
 	{
 		perror("ioctl");
 		exit(-1);
 	} 
-    close(fd);
+	close(fd);
 
-    my_mac = Mac((uint8_t *)ifr.ifr_hwaddr.sa_data); 
+	my_mac = Mac((uint8_t *)ifr.ifr_hwaddr.sa_data); 
 
-    return my_mac;
+	return my_mac;
 }
 
 int send_forward_packet(pcap_t* handle, EthIpTcpPacket* packet, Mac my_mac)
@@ -136,7 +75,7 @@ int send_backward_packet(pcap_t* handle, EthIpTcpPacket* packet, Mac my_mac)
 		EthIpTcpPacket backward_packet;
 		char msg[sizeof(MSG)];
 	} p;
-
+		
 	memcpy(&p.backward_packet, packet, sizeof(EthIpTcpPacket));
 	p.backward_packet.eth_.smac_ = my_mac;
 	p.backward_packet.eth_.dmac_ = packet->eth_.smac_;
